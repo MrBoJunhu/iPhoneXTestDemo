d2a87fc7153bd0d41f0af541a285b0cd920bf53a
BillBo <496081649@qq.com>
2017-11-24 11:03:58 +0800

    工程初始化

---
+++ iPhoneXDemo/iPhoneXDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneXDemo
+//
+//  Created by BillBo on 2017/11/24.
+//  Copyright © 2017年 BillBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
